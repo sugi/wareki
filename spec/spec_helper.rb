e80811fb9b173b2bf341a951cffb3c95cec1e465
Tatsuki Sugiura <sugi@nemui.org>
2015-10-29 08:25:06 +0900

    Add badge to README and tune condition.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,21 @@
 #
-require 'coveralls'
-Coveralls.wear!
-
 $:.unshift File.dirname(__FILE__)+'/../lib'
-require 'simplecov'
-require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  add_filter 'build-util/'
-  add_filter 'spec/'
+unless defined? JRUBY_VERSION
+  require 'coveralls'
+  Coveralls.wear!
+
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start do
+    add_filter 'build-util/'
+    add_filter 'spec/'
+  end
 end
 
 require 'wareki'
